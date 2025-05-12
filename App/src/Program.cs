e7299098be8065733a2b5c82844368f035d2da88
Kitsune <KyuubiRan@qq.com>
2025-05-12 18:51:27 +0800

    Attach progress when app start

--- App/src/Program.cs
+++ App/src/Program.cs
@@ -2,11 +2,25 @@
 
 #pragma warning disable CA1416
 
+using System.Diagnostics;
 using App.Ui;
 using App.Util;
 
-var watchedProcesses = new List<string>
-    { "dontstarve_steam_x64.exe", "dontstarve_dedicated_server_nullrenderer_x64.exe" };
+HashSet<string> watchedProcesses = ["dontstarve_steam_x64.exe", "dontstarve_dedicated_server_nullrenderer_x64.exe"];
+
+{
+    var procs = Process.GetProcesses();
+    var procNames = watchedProcesses.Select(x => x[..^4]).ToHashSet();
+    foreach (var proc in procs)
+    {
+        // Console.WriteLine($"Found process: {proc.ProcessName}(PID={proc.Id})");
+        if (procNames.Contains(proc.ProcessName))
+        {
+            ManagedConsole.CreateWindow(new ProcessWatcher.ProcessEventArgs(proc.ProcessName, (uint)proc.Id, true));
+            Console.WriteLine($"Found dst process: {proc.ProcessName}(PID={proc.Id})");
+        }
+    }
+}
 
 ProcessWatcher.ProcessEvent += (sender, eventArgs) =>
 {
